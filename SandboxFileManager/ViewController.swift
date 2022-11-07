fb65f2d51a2bc83fc429ac5205031b8f8304cea9
Nikita Byzov <spamsmails@icloud.com>
2022-11-07 23:09:20 +0600

    Initial Commit

---
+++ SandboxFileManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SandboxFileManager
+//
+//  Created by Nikita Byzov on 07.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
